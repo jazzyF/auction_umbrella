cc515d5394ddd5e10628fccfa109d42875c1f6f0
Fola Ayinde <folabi.ayinde@gmail.com>
2018-09-14 17:47:03 -0400

    Chp 9

--- apps/auction_web/lib/auction_web/router.ex
+++ apps/auction_web/lib/auction_web/router.ex
@@ -2,21 +2,22 @@ defmodule AuctionWeb.Router do
   use AuctionWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", AuctionWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    resources("/items", ItemController, only: [:index, :show])
   end
 
   # Other scopes may use custom stacks.
